34c037c7cb81c60878243edaffae4b0a4de55083
Stephane Fontaine <esselfe16@gmail.com>
2023-02-10 18:49:12 -0500

    0.1.3: fix white space bug

--- src/font.c
+++ src/font.c
@@ -9,12 +9,11 @@
 
 static GLubyte *font_data;
 static GLubyte *letter_data;
-//static unsigned int bytes_per_line = 3*884;
 static unsigned int bytes_per_line = 3*760;
 
 static void FontMakeLetter(unsigned char letter) {
 	unsigned int cnt, iter, offset;
-	for (cnt = 0, offset = (letter-31)*8*3; cnt < 3*8*16; cnt += 8*3) {
+	for (cnt = 0, offset = (letter-32)*8*3; cnt < 3*8*16; cnt += 8*3) {
 		for (iter = 0; iter < 24; iter++)
 			*(letter_data + cnt + iter) = *(font_data + offset + iter);
 		offset += bytes_per_line;
@@ -26,11 +25,11 @@ static void FontMakeLetter(unsigned char letter) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// Linear = smooth
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	// Nearest = crisp
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexImage2D (GL_TEXTURE_2D, 0, GL_RGB, 8, 16,
 				  0, GL_RGB, GL_UNSIGNED_BYTE, letter_data);
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -55,7 +54,7 @@ void FontRender(GLfloat x, GLfloat y, GLfloat z, char *text) {
 	glColor3f(0.7, 0.8, 0.9);
 	for (cnt = 0; cnt < strlen(text); cnt++,c++) {
 		glPushMatrix();
-		glBindTexture(GL_TEXTURE_2D, (*c)-32);
+		glBindTexture(GL_TEXTURE_2D, (*c)-31);
 		glTranslatef(x + cnt*0.08, y, z);
 		glBegin(GL_POLYGON);
 		glTexCoord2f(0.0, 1.0);
@@ -78,7 +77,7 @@ void FontRender2D(int x, int y, char *text) {
 	glColor4f(1.0, 1.0, 1.0, 1.0);
 	for (cnt = 0; cnt < strlen(text); cnt++,c++) {
 		glPushMatrix();
-		glBindTexture(GL_TEXTURE_2D, (*c)-32);
+		glBindTexture(GL_TEXTURE_2D, (*c)-31);
 		//glRasterPos2i(x + cnt*9, y);
 		glTranslatef((GLfloat)(x + cnt*8), (GLfloat)y, 1.0);
 		glBegin(GL_POLYGON);
