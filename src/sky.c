e9103e62add69378570882489a56152613c05324
Stephane Fontaine <esselfe16@gmail.com>
2023-02-14 00:34:57 -0500

    0.1.4: implement day and night

--- src/sky.c
+++ src/sky.c
@@ -4,7 +4,7 @@
 #include "nexus.h"
 
 GLuint sky_texture_1, sky_texture_2, sky_texture_3, sky_texture_4,
-	sky_list;
+	sky_list, daylight_list;
 
 void SkySetup(GLuint *id, char *filename) {
 	GLubyte *data = ImageFromPNGFile(2048, 2048, filename);
@@ -92,12 +92,74 @@ void SkyInit(void) {
 	
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glEndList();
+	
+	daylight_list = glGenLists(1);
+	glNewList(daylight_list, GL_COMPILE_AND_EXECUTE);
+	
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0, 1.0);
+	 glVertex3f(-975.0, -975.0, -975.0);
+	glTexCoord2f(1.0, 1.0);
+	 glVertex3f(-975.0, 975.0, -975.0);
+	glTexCoord2f(1.0, 0.0);
+	 glVertex3f(975.0, 975.0, -975.0);
+	glTexCoord2f(0.0, 0.0);
+	 glVertex3f(975.0, -975.0, -975.0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0, 1.0);
+	 glVertex3f(975.0, -975.0, -975.0);
+	glTexCoord2f(1.0, 1.0);
+	 glVertex3f(975.0, 975.0, -975.0);
+	glTexCoord2f(1.0, 0.0);
+	 glVertex3f(975.0, 975.0, 975.0);
+	glTexCoord2f(0.0, 0.0);
+	 glVertex3f(975.0, -975.0, 975.0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0, 1.0);
+	 glVertex3f(975.0, -975.0, 975.0);
+	glTexCoord2f(1.0, 1.0);
+	 glVertex3f(975.0, 975.0, 975.0);
+	glTexCoord2f(1.0, 0.0);
+	 glVertex3f(-975.0, 975.0, 975.0);
+	glTexCoord2f(0.0, 0.0);
+	 glVertex3f(-975.0, -975.0, 975.0);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0, 1.0);
+	 glVertex3f(-975.0, -975.0, 975.0);
+	glTexCoord2f(1.0, 1.0);
+	 glVertex3f(-975.0, 975.0, 975.0);
+	glTexCoord2f(1.0, 0.0);
+	 glVertex3f(-975.0, 975.0, -975.0);
+	glTexCoord2f(0.0, 0.0);
+	 glVertex3f(-975.0, -975.0, -975.0);
+	glEnd();
+	
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glEndList();
 }
 
+GLfloat daylight_amount;
+int daylight_up = 1;
 void SkyRender(void) {
 	glPushMatrix();
 	glTranslatef(cam.x, cam.y, cam.z);
 	glCallList(sky_list);
+	glColor4f(0.6, 0.6, 0.8, daylight_amount);
+	glCallList(daylight_list);
 	glPopMatrix();
+	
+	if (daylight_up) daylight_amount += 0.0001 * delta_move;
+	else daylight_amount -= 0.0001 * delta_move;
+	
+	if (daylight_up && daylight_amount >= 1.0)
+		daylight_up = 0;
+	else if (!daylight_up && daylight_amount <= 0.0)
+		daylight_up = 1;
 }
 
