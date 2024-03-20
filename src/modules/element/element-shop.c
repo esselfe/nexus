a17293ebbe3ec9e455e68de0b7ef0112c1bc5664
Stephane Fontaine <esselfe16@gmail.com>
2024-03-20 09:45:14 -0400

    0.2.3: move element shop rendering to a display list

--- src/modules/element/element-shop.c
+++ src/modules/element/element-shop.c
@@ -7,23 +7,21 @@ GLfloat shop_x = -50.0, shop_y = 0.0, shop_z = 50.0;
 GLfloat shop_width = 20.0, shop_height = 4.0, shop_depth = 10.0;
 GLuint shop_texture_id_planks_001, shop_texture_id_planks_002,
     shop_texture_id_planks_003, shop_texture_id_planks_004;
+GLuint shop_display_list;
 
 void ElementShopInit(void) {
     ElementTextureCreate(&shop_texture_id_planks_001, "images/planks-001-128.png");
     ElementTextureCreate(&shop_texture_id_planks_002, "images/planks-002-128.png");
     ElementTextureCreate(&shop_texture_id_planks_003, "images/planks-003-128.png");
     ElementTextureCreate(&shop_texture_id_planks_004, "images/planks-004-128.png");
-}
 
-void ElementShopRender(void) {
-    glPushMatrix();
-    glScalef(1.0, 1.0, -1.0);
-    glTranslatef(shop_x, shop_y, shop_z);
+    // Create the shop display list
+    shop_display_list = glGenLists(1);
+    glNewList(shop_display_list, GL_COMPILE);
 
     // Front
     glBindTexture(GL_TEXTURE_2D, shop_texture_id_planks_001);
     glBegin(GL_POLYGON);
-    glColor4f(0.2, 0.25, 0.3, 0.8);
     glTexCoord2f(0.0, 0.0);
     glVertex3f(-(shop_width/2.0), 0.0, -(shop_depth/2.0));
     glTexCoord2f(1.0, 0.0);
@@ -36,7 +34,6 @@ void ElementShopRender(void) {
 
     glBindTexture(GL_TEXTURE_2D, shop_texture_id_planks_002);
     glBegin(GL_POLYGON);
-    glColor4f(0.2, 0.25, 0.3, 0.8);
     glTexCoord2f(0.0, 0.0);
     glVertex3f(-(shop_width/2.0) + 1.0, shop_height - 1.0, -(shop_depth/2.0));
     glTexCoord2f(1.0, 0.0);
@@ -49,7 +46,6 @@ void ElementShopRender(void) {
 
     glBindTexture(GL_TEXTURE_2D, shop_texture_id_planks_003);
     glBegin(GL_POLYGON);
-    glColor4f(0.2, 0.25, 0.3, 0.8);
     glTexCoord2f(0.0, 0.0);
      glVertex3f(-(shop_width/2.0) + 3.0, 0.0, -(shop_depth/2.0));
     glTexCoord2f((shop_width - 3.0)/4.0, 0.0);
@@ -63,7 +59,6 @@ void ElementShopRender(void) {
     // Left side
     glBindTexture(GL_TEXTURE_2D, shop_texture_id_planks_004);
     glBegin(GL_POLYGON);
-    glColor4f(0.2, 0.25, 0.3, 0.8);
     glTexCoord2f(0.0, 0.0);
     glVertex3f(-(shop_width/2.0), 0.0, shop_depth/2.0);
     glTexCoord2f(shop_depth/2.0, 0.0);
@@ -77,7 +72,6 @@ void ElementShopRender(void) {
     // Right side
     glBindTexture(GL_TEXTURE_2D, shop_texture_id_planks_002);
     glBegin(GL_POLYGON);
-    glColor4f(0.2, 0.25, 0.3, 0.8);
     glTexCoord2f(0.0, 0.0);
     glVertex3f(shop_width/2.0, 0.0, shop_depth/2.0);
     glTexCoord2f(shop_depth/2.0, 0.0);
@@ -91,7 +85,6 @@ void ElementShopRender(void) {
     // Back side
     glBindTexture(GL_TEXTURE_2D, shop_texture_id_planks_003);
     glBegin(GL_POLYGON);
-    glColor4f(0.2, 0.25, 0.3, 0.8);
     glTexCoord2f(0.0, 0.0);
     glVertex3f(-(shop_width/2.0), 0.0, shop_depth/2.0);
     glTexCoord2f(shop_width/4.0, 0.0);
@@ -105,7 +98,6 @@ void ElementShopRender(void) {
     // Top side
     glBindTexture(GL_TEXTURE_2D, shop_texture_id_planks_004);
     glBegin(GL_POLYGON);
-    glColor4f(0.2, 0.25, 0.3, 0.8);
     glTexCoord2f(0.0, 0.0);
     glVertex3f(-(shop_width/2.0), shop_height, -(shop_depth/2.0));
     glTexCoord2f(shop_width/4.0, 0.0);
@@ -116,6 +108,17 @@ void ElementShopRender(void) {
     glVertex3f(-(shop_width/2.0), shop_height, shop_depth/2.0);
     glEnd();
     glBindTexture(GL_TEXTURE_2D, 0);
+
+    glEndList();
+}
+
+void ElementShopRender(void) {
+    glPushMatrix();
+    glScalef(1.0, 1.0, -1.0);
+    glTranslatef(shop_x, shop_y, shop_z);
+    glColor4f(0.2, 0.25, 0.3, 1.0);
+
+    glCallList(shop_display_list);
 
     glPopMatrix();
 }
